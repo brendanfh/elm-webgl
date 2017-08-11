557089f4feb1b3ea3a7308e890168f853eafcd94
Brendan <brendan.f.hansen@gmail.com>
2017-08-11 22:38:06 +0000

    added gl capabilities and gl rendering

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,11 @@
 module Main exposing (..)
 
+import AnimationFrame
 import Html exposing (Html)
 import Html.Attributes as HA
+import Math.Vector4 as Vec4 exposing (vec4, Vec4)
+import Math.Vector3 as Vec3 exposing (vec3, Vec3)
+import Math.Vector2 as Vec2 exposing (vec2, Vec2)
 import WebGL as GL
 
 
@@ -11,7 +15,7 @@ type alias Model =
 
 type Msg
     = NoOp
-    | Test
+    | Tick Float
 
 
 init : {} -> ( Model, Cmd Msg )
@@ -27,16 +31,115 @@ update msg model =
 view : Model -> Html Msg
 view model =
     GL.toHtmlWith
-        [ GL.clearColor 1 0 0 1 ]
+        [ GL.clearColor 0 0 0 1 ]
         [ HA.width 640
         , HA.height 480
         ]
-        []
+        [ render (drawEntities sampleScene)
+        ]
+
+
+type alias Entity =
+    { x : Float
+    , y : Float
+    , w : Float
+    , h : Float
+    , r : Float
+    , g : Float
+    , b : Float
+    , a : Float
+    }
+
+
+sampleScene : List Entity
+sampleScene =
+    let
+        func i =
+            let
+                x =
+                    (toFloat <| i % 50) * 2 - 50
+
+                y =
+                    (toFloat <| i // 50) * 2 - 50
+            in
+                Entity (x / 50) (y / 50) (1 / 51) (1 / 51) 1 0 0 1
+    in
+        List.map func (List.range 0 2500)
+
+
+type alias Vertex =
+    { position : Vec2
+    , color : Vec4
+    }
+
+
+type alias Triangle v =
+    ( v, v, v )
+
+
+drawEntity : Entity -> List (Triangle Vertex)
+drawEntity { x, y, w, h, r, g, b, a } =
+    let
+        color =
+            vec4 r g b a
+    in
+        [ ( Vertex (vec2 x y) color
+          , Vertex (vec2 x (y + h)) color
+          , Vertex (vec2 (x + w) (y + h)) color
+          )
+        , ( Vertex (vec2 x y) color
+          , Vertex (vec2 (x + w) (y + h)) color
+          , Vertex (vec2 (x + w) y) color
+          )
+        ]
+
+
+drawEntities : List Entity -> List (Triangle Vertex)
+drawEntities entities =
+    List.concatMap drawEntity entities
+
+
+render : List (Triangle Vertex) -> GL.Entity
+render triangles =
+    GL.entity vertexShader fragmentShader (GL.triangles triangles) {}
+
+
+type alias Varyings =
+    { vColor : Vec4
+    }
+
+
+vertexShader : GL.Shader Vertex {} Varyings
+vertexShader =
+    [glsl|
+        attribute vec2 position;
+        attribute vec4 color;
+
+        varying vec4 vColor;
+
+        void main() {
+            vColor = color;
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    |]
+
+
+fragmentShader : GL.Shader {} {} Varyings
+fragmentShader =
+    [glsl|
+        precision mediump float;
+
+        varying vec4 vColor;
+
+        void main() {
+            gl_FragColor = vColor;
+        }
+    |]
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    AnimationFrame.diffs Tick
 
 
 main : Program {} Model Msg
